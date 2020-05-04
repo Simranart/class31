b7eb58a5468fe2e99b8cf9563df8463317e9a091
Simran <simranrs2225@gmail.com>
2020-05-04 21:03:35 +0530

    add files

--- Bird.js
+++ Bird.js
@@ -2,12 +2,31 @@ class Bird extends BaseClass {
   constructor(x,y){
     super(x,y,50,50);
     this.image = loadImage("sprites/bird.png");
+    this.smokeImage =loadImage("sprites/smoke.png");
+    this.trajectery = [];
+
+
   }
 
   display() {
     //this.body.position.x = mouseX;
     //this.body.position.y = mouseY;
-
+    
     super.display();
+console.log(this.body.velocity.x)
+
+if(this.body.position.x>200 && this.body.velocity.x<10){
+  var position = [this.body.position.x,this.body.position.y]
+  this.trajectery.push(position);
+
+}
+for(var i = 0;i < this.trajectery.length;i++){
+     image(this.smokeImage,this.trajectery[i][0],this.trajectery[i][1])
+     
+     
+
+    }
   }
 }
+
+
